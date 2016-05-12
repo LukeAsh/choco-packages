2ab27a78121aca7db27525ada28f09198a343b6d
Lukasz Walach <lukasz.walach@optofidelity.com>
2016-05-12 10:42:34 +0300

    Icon and package for v8.1

---
+++ freefilesync/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'freefilesync'
+$installerType = 'EXE'
+$url = 'http://www.freefilesync.org/download/w2wvxxvauqfik7d/FreeFileSync_8.1_Windows_Setup.exe'
+$silentArgs = '/silent'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
